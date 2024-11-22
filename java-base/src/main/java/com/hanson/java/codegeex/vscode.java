57638cfd52accbf40a42c59c4673d501964ca374
huang.hanson/黄忠_沪_HQDV <hanson.huang@51job.com>
2024-11-22 18:59:46 +0800

    vscode初始化提交

---
+++ java-base/src/main/java/com/hanson/java/codegeex/vscode.java
@@ -0,0 +1,9 @@
+package com.hanson.java.codegeex;
+
+public class vscode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
---
+++ java-base/src/test/java/com/hanson/java/JavaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanson.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
