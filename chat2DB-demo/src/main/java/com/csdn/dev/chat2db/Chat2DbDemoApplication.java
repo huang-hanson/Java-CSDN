217d796e72f998a025b651636c5d8389697aeccd
huang.hanson/黄忠_沪_HQDV <hanson.huang@51job.com>
2025-01-08 18:25:17 +0800

    add chat2db

---
+++ chat2DB-demo/src/main/java/com/csdn/dev/chat2db/Chat2DbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.csdn.dev.chat2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2DbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat2DbDemoApplication.class, args);
+    }
+
+}
---
+++ chat2DB-demo/src/test/java/com/csdn/dev/chat2db/Chat2DbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdn.dev.chat2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2DbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
