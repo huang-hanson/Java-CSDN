0f7f505a6eeb57e9ca9a8dcf45066764bb52e6fe
huang.hanson/黄忠_沪_HQDV <hanson.huang@51job.com>
2025-03-11 13:26:11 +0800

    remove nacos1.4.3 source

--- java-base/src/test/java/com/hanson/java/BastTest.java
+++ java-base/src/test/java/com/hanson/java/BastTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -47,5 +48,19 @@ public class BastTest {
             stringRedisTemplate.delete("track_media_0ed5103f88f9b3417c8cba24b8cc268d_0ed5103f88f9b3417c8cba24b8cc268d_0_0_0_0");
             log.info("track_media_0ed5103f88f9b3417c8cba24b8cc268d_0ed5103f88f9b3417c8cba24b8cc268d_0_0_0_0 删除成功");
         }
+    }
+
+    @Test
+    void test_regex() {
+        String NUMBER_PATTERN = "^\\d+$";
+        boolean matches = "abc123".matches(NUMBER_PATTERN);
+        log.info("matches: {}", matches);
+        matches = "123".matches(NUMBER_PATTERN);
+        log.info("matches: {}", matches);
+        String[] split = "123.abc123.234".split(".");
+        boolean b = Arrays.stream(split).allMatch(s -> s.matches(NUMBER_PATTERN));
+        boolean c = Arrays.stream(split).anyMatch(s -> s.matches(NUMBER_PATTERN));
+        boolean d = Arrays.stream(split).noneMatch(s -> s.matches(NUMBER_PATTERN));
+        log.info("b: {},c:{},d:{}", b, c, d);
     }
 }
